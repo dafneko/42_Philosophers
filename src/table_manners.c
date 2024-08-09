ab8342f4d7ef732913344decbaa762bbf6b8463b
dafneko <defne.koca@outlook.com>
2024-08-09 04:24:47 +0200

    deleted unused function

--- src/table_manners.c
+++ src/table_manners.c
@@ -6,7 +6,7 @@
 /*   By: dkoca <dkoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 00:23:17 by dkoca             #+#    #+#             */
-/*   Updated: 2024/08/09 03:07:48 by dkoca            ###   ########.fr       */
+/*   Updated: 2024/08/09 04:24:14 by dkoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,4 @@ void	put_down_fork(t_philo *philo)
 		pthread_mutex_unlock(philo->left_fork);
 		pthread_mutex_unlock(philo->right_fork);
 	}
-}
-
-void	wait_for_others(t_philo *philo)
-{
-	pthread_mutex_lock(philo->arg.meal_mtx);
-	if (look_at_clock() < *philo->start_time)
-	{
-		usleep(500);
-	}
-	pthread_mutex_unlock(philo->arg.meal_mtx);
 }
